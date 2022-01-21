d983e3de8912d8238b7f0033fc2060f66c326962
Nathan <nathanszekutb@hotmail.com>
2022-01-21 19:17:25 -0300

    Aula 1

---
+++ media.js
@@ -0,0 +1,9 @@
+// trabalhando com arrays - faça a média das
+// notas 10 6.5 7.5 e 8 de um aluno
+
+const notas = [10, 6.5, 7.5, 8];
+
+let media = (notas[0]+notas[1]+notas[2]+notas[3])
+/notas.length;
+
+console.log(media);
