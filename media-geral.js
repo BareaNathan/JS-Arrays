404a6990cb9d15a11ab1e7597e640d2b41f1db39
Nathan <nathanszekutb@hotmail.com>
2022-01-22 19:36:13 -0300

    Aula 5 - Final

---
+++ media-geral.js
@@ -0,0 +1,17 @@
+const salaJS = [7,8,8,7,10,6.5,4,10,7];
+const salaJava = [6,5,8,9,5,6];
+const salaPython = [7,3.5,8,9.5];
+
+function mediaSala(notasDaSala){
+    const somaDasNotas = notasDaSala.reduce((acum, atual) => atual + acum,0)
+    return somaDasNotas/notasDaSala.length;
+}
+
+console.log(`Média da Sala de JavaScript ${mediaSala(salaJS)}`)
+console.log(`Média da Sala de Java ${mediaSala(salaJava)}`)
+console.log(`Média da Sala de Python ${mediaSala(salaPython)}`)
+
+const notas = [10,6.5,8,7];
+const media = notas.reduce((acum,atual) => atual + acum,0)/notas.length;
+
+console.log(media);
